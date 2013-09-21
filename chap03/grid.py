137cb17031171c8f6d562766064dbff68b05fc41
Victoria Preston <victoria.preston@students.olin.edu>
2013-09-21 15:24:54 -0400

    Chapter 3 Grid Program

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,24 @@
-"""Solution to an exercise in Think Python.
-
-Author:
 """
+Solution to an exercise in Think Python.
 
-print '+ - - - -'
+Author: Victoria Preston
+
+This code is the grid exercise in Chapter 3 of Think Python.  The called function takes arguments for the dimensions of the grid, and then prints out the appropriate shape for those arguments.
+"""
+def draw_four(n):
+	print n
+	print n
+	print n
+	print n
+
+def draw_grid(row, column):
+	segment1 = '* - - - - '
+	segment2 = '|         '
+	finstar = '*'
+	fincol = '|'
+	for i in range(row):
+		print segment1 * column + finstar
+		do_four(segment2 * column + fincol)
+	print segment1 * column + finstar
+
+draw_grid(5,4)
